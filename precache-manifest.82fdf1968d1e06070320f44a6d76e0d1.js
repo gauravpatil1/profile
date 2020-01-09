086f634e2f9f4d3a7e4da590bb1bf069d1aee3e8
gauravpatil1 <thegaurav49@gmail.com>
2020-01-09 15:48:53 +0530

    Updates

---
+++ precache-manifest.82fdf1968d1e06070320f44a6d76e0d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f454d5d250035ebb6dd2489caa6d9bb",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "2e975bd8a2dc0419e1be",
+    "url": "/profile/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "923dd5cc780e68f87d64",
+    "url": "/profile/static/js/2.14023ba9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/profile/static/js/2.14023ba9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e975bd8a2dc0419e1be",
+    "url": "/profile/static/js/main.825c3986.chunk.js"
+  },
+  {
+    "revision": "9ea71b400ca9a5d22514",
+    "url": "/profile/static/js/runtime-main.91af3a0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
