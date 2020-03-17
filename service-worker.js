53dd671cbe17b8618b93b564729684c134f21066
gauravpatil1 <patilgaurav309@gmail.com>
2020-03-17 19:35:27 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/profile/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
